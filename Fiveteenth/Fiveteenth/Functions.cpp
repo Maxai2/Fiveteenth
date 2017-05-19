a352153ff64e576c192e4de0c90997a69ff59510
Хамелеон <maxali946@gmail.com>
2017-05-20 00:26:48 +0400

    newFiveteenth

---
+++ Fiveteenth/Fiveteenth/Functions.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <Windows.h>
+#include <time.h>
+
+using namespace std;
+
+HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void frame()
+{
+	int rowS = 4, rowF = 21, colS = 15, colF = 32;
+
+	for (short i = rowS; i <= rowF; i++)
+	{
+		for (short j = colS; j <= colF; j++)
+		{
+			if (i == rowS || i == rowF)
+			{
+				SetConsoleCursorPosition(h, { j, i });
+
+				cout << char(178);
+			}
+			else if (rowS < i && i < rowF && (j == colS || j == colF))
+			{
+				SetConsoleCursorPosition(h, { j, i });
+
+				cout << char(178);
+			}
+		}
+	}
+}
+
+void symbols()
+{
+	int rowS = 6, rowF = 19, colS = 16, colF = 31, tempThree = 16, tempOne = 16;
+
+	for (short i = rowS; i <= rowF; i++)
+	{
+		for (short j = colS; j <= colF; j++)
+		{
+			if (i % 2 == 0 && j == tempThree + 3 && j != colF)
+			{
+				SetConsoleCursorPosition(h, { j, i });
+				tempThree = j;
+				cout << char(179);
+			}
+			else if (i % 2 != 0 && j == tempOne + 2 && j != colF)
+			{
+				SetConsoleCursorPosition(h, { j, i });
+				tempOne = j;
+				cout << char(196);
+			}
+		}
+		tempThree = 16; tempOne = 16;
+	}
+}
---
+++ Fiveteenth/Fiveteenth/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Functions.h"
+
+using namespace std;
+
+int main()
+{
+	symbols();
+	frame();
+
+	cout << endl << endl;
+}
