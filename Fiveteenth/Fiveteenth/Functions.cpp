b30e6ca68953ed2c822fa9a6bb028edd16f51225
Хамелеон <maxali946@gmail.com>
2017-05-22 21:13:03 +0400

    Finish

--- Fiveteenth/Fiveteenth/Functions.cpp
+++ Fiveteenth/Fiveteenth/Functions.cpp
@@ -74,9 +74,9 @@ void initialArr(int arr[], int length)
 			{
 				have = true;
 				j = temp - 1;
-			}	
-		}		
-		
+			}
+		}
+
 		if (have == false)
 			temp++;
 		else
@@ -99,7 +99,7 @@ void show(int arr[], int length)
 			if (arr[i] * 10 < 100 && arr[i] != 0)
 				cout << 0 << arr[i];
 			else if (arr[i] == 0)
-				cout << " ";
+				cout << "  ";
 			else
 				cout << arr[i];
 
@@ -134,17 +134,88 @@ int findZero(int arr[])
 
 bool cheker(int arr[], int length)
 {
-	for (int i = 0; i < length; i += 2)
+	int temp = 0;
+	for (int i = 0; i < length; i++)
 	{
-		if (arr[i] > arr[i + 1])
-			return false;
+		if (arr[i] < arr[i + 1])
+			temp++;
+	}
+
+	if (temp == 14 && arr[15] == 0)
+	{
+		temp = 0;
+		return true;
+	}
+	else
+	{
+		temp = 0;
+		return false;
+	}
+
+}
+
+void example(int arr[])
+{
+	for (int i = 0; i < 16; i++)
+	{
+		if (i != 15)
+			arr[i] = i + 1;
 		else
-			return true;
+			arr[i] = 0;
 	}
 }
+
+void cheatCode(int arr[])
+{
+	int sec = 7, fNum = 0, sNum = 0, temp = 0;
+	while (sec != -1)
+	{
+		SetConsoleCursorPosition(h, { 0, 0 });
+		cout << "          You have a 10 second to use cheat: " << sec << endl;
+
+		cout << "First number(1-16): ";
+		if (kbhit())
+		{
+			cin >> fNum;
+			cout << "Second number(1-16): ";
+			cin >> sNum;
+
+			fNum--; sNum--;
+
+			temp = arr[fNum];
+			arr[fNum] = arr[sNum];
+			arr[sNum] = temp;
+			show(arr, 16);
+			symbols();
+			SetConsoleCursorPosition(h, { 20, 1 });
+			cout << "   ";
+			SetConsoleCursorPosition(h, { 20, 2 });
+			cout << "   ";
+		}
+
+		Sleep(1000);
+		sec--;
+		SetConsoleCursorPosition(h, { 33, 0 });
+		cout << "  ";
+	}
+}
+
+void clearCheat()
+{
+	for (short i = 0; i < 3; i++)
+	{
+		SetConsoleCursorPosition(h, { 0,i });
+		cout << "\t\t\t\t\t\t";
+	}
+
+}
+
 void move(int arr[])
 {
 	int key, temp = 0;
+	bool cheat = true;
+	SetConsoleCursorPosition(h, { 0,0 });
+	cout << "Input \'`\' for cheat(7 sec) but use it at once";
 	while (true)
 	{
 		show(arr, 16);
@@ -155,25 +226,48 @@ void move(int arr[])
 		if (key == 224)
 			key = getch();
 
-		if (key == 72) // Up
+		if (key == 72 && temp < 12) // Up
 		{
 			arr[temp] = arr[temp + 4];
 			arr[temp + 4] = 0;
 		}
-		else if (key == 80) // Down
+		else if (key == 80 && temp > 3) // Down
 		{
 			arr[temp] = arr[temp - 4];
 			arr[temp - 4] = 0;
 		}
-		else if (key == 75) // Left
+		else if (key == 75 && temp != 3 && temp != 7 && temp != 11 && temp != 15) // Left
 		{
 			arr[temp] = arr[temp + 1];
 			arr[temp + 1] = 0;
 		}
-		else if (key == 77) // Right
+		else if (key == 77 && temp % 4 != 0) // Right
 		{
 			arr[temp] = arr[temp - 1];
 			arr[temp - 1] = 0;
+		}
+		else if (key == 96 && cheat == true) // ` 
+		{
+			cheatCode(arr);
+			cheat = false;
+			clearCheat();
+		}
+		else if (key == 23) // ctrl + W
+		{
+			example(arr);
+			show(arr, 16);
+			symbols();
+		}
+		else if (key == 27) // esc
+		{
+			break;
+		}
+
+		if (cheker(arr, 16))
+		{
+			break;
+			show(arr, 16);
+			symbols();
 		}
 	}
 }
